d5539d0cc68bfbbf2e99d13cdc3376b8c6eebaaa
MarceloR123 <110556664+MarceloR123@users.noreply.github.com>
2022-08-04 21:17:44 -0500

    Add files via upload

---
+++ ejercicio días del año.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  4 18:44:32 2022
+
+@author: LENOVO
+"""
+min=-10
+max=10
+for v in range(-10,11): 
+    n= int(input("ingrese un número dentro del rango -10,10")) 
+    if : n is < or > than v:
+        print("El valor no esta en el rango permitido(-10,10)")
+    elif n is str:
+        print("Error en el ingreso")
+    v=readint(enter a number from -10 to 10:",-10,10)
+              input("enter a number")
+              print("The number is:",v)
+              
